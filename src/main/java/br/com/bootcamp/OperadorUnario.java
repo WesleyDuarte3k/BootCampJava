ad0df55e8ccbccb6b94ffb0ad34e61bdfe8e7d21
Wereis <wesleyduarteribeiro013@gmail.com>
2024-04-26 18:24:54 -0300

    adicionando OperadorUnario

---
+++ src/main/java/br/com/bootcamp/OperadorUnario.java
@@ -0,0 +1,11 @@
+package br.com.bootcamp;
+
+public class OperadorUnario {
+	public static void main(String[] args) {
+		int numero = 9;
+		System.out.println(numero);
+		numero = - numero;
+		System.out.println(numero);
+		numero = numero * -1;
+	}
+}
