79b0d730f877c51c7f0cb37dd53c16be034e843c
Wereis <wesleyduarteribeiro013@gmail.com>
2024-04-26 19:19:15 -0300

    adicionando OperadorRelacional

---
+++ src/main/java/br/com/bootcamp/OperadorRelacional.java
@@ -0,0 +1,27 @@
+package br.com.bootcamp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OperadorRelacional {
+	public static void main(String[] args) {
+		List<Integer> crescente = new ArrayList<>();
+		List<Integer> decrescente = new ArrayList<>();
+		int tamanhoDaLista = 16;
+		int tamanhoDaOtaLista = 16;
+		for (int i = 0; i < tamanhoDaLista; i++){
+			crescente.add(i);
+			decrescente.add(--tamanhoDaOtaLista);
+			boolean resultado = tamanhoDaOtaLista == (i-1)?true : false;
+			if (resultado != false){
+				System.out.println(--i);
+				System.out.println(tamanhoDaOtaLista);
+				System.out.println("os numeros são iguais");
+			}
+
+
+		}
+		System.out.println(crescente.toString());
+		System.out.println(decrescente.toString());
+	}
+}
