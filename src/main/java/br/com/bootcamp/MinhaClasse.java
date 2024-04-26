37211542d89b4d8a4c1d5f020bf1d76f990f6530
Wereis <wesleyduarteribeiro013@gmail.com>
2024-04-26 13:11:52 -0300

    Criação da classe minha classe

---
+++ src/main/java/br/com/bootcamp/MinhaClasse.java
@@ -0,0 +1,13 @@
+package br.com.bootcamp;
+
+public class MinhaClasse {
+	public static void main(String[] args) {
+		final String BR = "Brasil";
+		final int ESTADO_DO_BRASIL = 27;
+
+		String meuNome = "Wesley";
+		int anoDeFrabicacao = 2024;
+
+		anoDeFrabicacao = 2018;
+	}
+}
