c68b1ec9918e1dc05ebc3bf8bcfa21530a00bbf4
Wereis <wesleyduarteribeiro013@gmail.com>
2024-04-26 20:50:58 -0300

    adicionando OperadorLogico

--- src/main/java/br/com/bootcamp/OperadorRelacional.java
+++ src/main/java/br/com/bootcamp/OperadorRelacional.java
@@ -7,19 +7,22 @@ public class OperadorRelacional {
 	public static void main(String[] args) {
 		List<Integer> crescente = new ArrayList<>();
 		List<Integer> decrescente = new ArrayList<>();
+
 		int tamanhoDaLista = 16;
 		int tamanhoDaOtaLista = 16;
+
+		int somaTamanhosDaLista = tamanhoDaLista + tamanhoDaOtaLista;
+
 		for (int i = 0; i < tamanhoDaLista; i++){
 			crescente.add(i);
 			decrescente.add(--tamanhoDaOtaLista);
 			boolean resultado = tamanhoDaOtaLista == (i-1)?true : false;
+
 			if (resultado != false){
 				System.out.println(--i);
 				System.out.println(tamanhoDaOtaLista);
 				System.out.println("os numeros são iguais");
 			}
-
-
 		}
 		System.out.println(crescente.toString());
 		System.out.println(decrescente.toString());
---
+++ src/main/java/br/com/bootcamp/OperadoresLogicos.java
@@ -0,0 +1,18 @@
+package br.com.bootcamp;
+
+public class OperadoresLogicos {
+	public static void main(String[] args) {
+		int numero1 = 2;
+		int numero2 = 2;
+		int numero3 = 4;
+		int numero4 = 5;
+
+		if (numero1 == numero2 && numero3 != numero4){
+			numero1 ++;
+			if (numero1 > numero2 || numero4 > numero3){
+				numero1 --;
+				System.out.println(numero1);
+			}
+		}
+	}
+}
